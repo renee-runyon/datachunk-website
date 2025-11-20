d1365821fedad34a6113eaa011d00f5847e47c95
renee-ruyon <reneerunyon@gmail.com>
2025-11-20 14:37:53 -0500

    Fix layout issue after form submit

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,11 +1,17 @@
 "use client"
 import Sidebar from "@/components/Sidebar";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
 	return (
-		<>
-			<Sidebar />
-			{children}
-		</>
+		<html lang="en">
+			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+			</head>
+			<body>
+				<Sidebar />
+				{children}
+			</body>
+		</html>
 	);
 }
